025f2dc21b17fcba94d717a9448f7bc51daf8c3e
Igor Yalovoy <to@ylv.io>
2022-10-08 16:30:48 -0500

    Fix paths

--- src/WUSD.sol
+++ src/WUSD.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "openzeppelin-contracts/contracts/access/AccessControl.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "openzeppelin-contracts/access/AccessControl.sol";
+import "openzeppelin-contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 
 contract WrappedUSD is ERC20, ERC20Burnable, AccessControl, ERC20FlashMint {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
