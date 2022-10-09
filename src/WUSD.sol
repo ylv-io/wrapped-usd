a97db4d95e0f06493bf11f222e2af3ac300fbc48
Igor Yalovoy <to@ylv.io>
2022-10-08 19:07:25 -0500

    Add events

--- src/WUSD.sol
+++ src/WUSD.sol
@@ -10,6 +10,9 @@ import 'openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol';
 contract WrappedUSD is ERC20, AccessControl, ERC20FlashMint {
     using SafeERC20 for IERC20;
 
+    event Mint(address indexed to, address indexed coin, uint256 value);
+    event Burn(address indexed from, address indexed coin, uint256 value);
+
     constructor() ERC20("Wrapped USD", "WUSD") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
@@ -17,10 +20,12 @@ contract WrappedUSD is ERC20, AccessControl, ERC20FlashMint {
     function mint(address _coin, uint256 _amountIn) public {
         IERC20(_coin).safeTransferFrom(msg.sender, address(this), _amountIn);
         _mint(msg.sender, _amountIn);
+        emit Mint(msg.sender, _coin, _amountIn);
     }
 
     function burn(address _coin, uint256 _amountIn) public {
         _burn(msg.sender, _amountIn);
         IERC20(_coin).safeTransfer(msg.sender, _amountIn);
+        emit Burn(msg.sender, _coin, _amountIn);
     }
 }
